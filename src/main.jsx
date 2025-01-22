e6efcdfb31e119a40deabdd4c3c055f140e7c421
Chetan <227r1a66e1@cmrtc.ac.in>
2025-01-23 04:01:29 +0530

    fixed routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { createBrowserRouter, Outlet, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Blogs from './pages/Blogs.jsx'
 import About from './pages/About.jsx'
@@ -12,42 +12,41 @@ import LoginPage from './pages/LoginPage.jsx'
 
 const router = createBrowserRouter([
   {
-    path:"/",
-    element: <App/>,
-    children:[
+    path: "/",
+    element: <App />, // This will render the App component which contains Navbar and Footer
+    children: [
       {
-        path:'/authorize',
-        element:<LoginPage/>
+        path: '/',  // This renders Home when you visit the root path
+        element: <Home />,
       },
       {
-        path:'/home',
-        element:<Home/>
+        path: '/home',  // Also renders Home when you visit the /home path
+        element: <Home />,
       },
       {
-        path: "/blogs",
-        element:<Blogs />
+        path: '/authorize',
+        element: <LoginPage />,
+      },
+      {
+        path: '/blogs',
+        element: <Blogs />,
       },
       {
         path: '/events',
-        element: <Events/>
+        element: <Events />,
       },
       {
         path: '/about',
-        element: <About />
+        element: <About />,
       },
       {
         path: '/communities',
-        element: <Communities/>
-      }
-    ]
+        element: <Communities />,
+      },
+    ],
   },
-  
-]
-)
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-    <RouterProvider router={router}>
-    <App/>  
-    </RouterProvider>
-  /* </React.StrictMode>, */
-)
+  <RouterProvider router={router} />
+);
